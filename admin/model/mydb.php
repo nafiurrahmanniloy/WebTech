c1c578028ea7de1a1d74596b8b71cb7bb4785fe3
Nafiur Rahman <nafiurrahman03112001@gmail.com>
2024-03-19 08:27:45 +0600

    Update mydb.php

--- admin/model/mydb.php
+++ admin/model/mydb.php
@@ -24,6 +24,33 @@ class Model
         return $result;
     }
 }
-
+<?php
+class model{
+    function openConn(){
+        $conn = new mysqli("localhost","root","","pp_db_04");
+        return $conn;
+    }
+    function addUser($conn,$table,$name,$email,$password,$role,$address,$phone){
+      $insertSql = "INSERT INTO $table(name,email,password,role,address,phone) VALUES ('$name','$email','$password','$role','$address','$phone')";
+      $result = $conn->query($insertSql);
+      return $result;
+    }
+    function showUser($conn,$table){
+      $showQuery = "SELECT * FROM $table";
+      $result = $conn->query($showQuery);
+      return $result; 
+    }
+    function deleteUser($conn,$table,$id){
+      $deleteSql = "DELETE FROM $table WHERE id=$id";
+      $result = $conn->query($deleteSql);
+      return $result;
+    }
+    function updateUser($conn,$table,$id,$name,$email,$password,$role,$address,$phone){
+      $updateSql = "UPDATE $table SET name='$name',email='$email',password='$password',role='$role',address='$address',phone='$phone' WHERE id=$id";
+      $result = $conn->query($updateSql);
+      return $result;
+    }
+}
+?>
 
 ?>
